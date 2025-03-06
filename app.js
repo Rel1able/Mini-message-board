83047ecfed058c726a40d7f1424474f85eef5699
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-03-06 07:57:04 +0100

    added try/catch blocks, fixed issue with favicon

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ app.use(express.static(assetsPath));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use("/new", newMessageRouter);
-app.use("/", messagesRouter);
-
 
 app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.use("/new", newMessageRouter);
+app.use("/", messagesRouter);
 
 
 const PORT = process.env.PORT || 3000;
--- db/queries.js
+++ db/queries.js
@@ -2,20 +2,34 @@ require("dotenv").config();
 const pool = require("./pool");
 
 async function getMessagesFromDB() {
-    const { rows } = await pool.query('SELECT * FROM messages');
-    return rows;
+    try {
+        const { rows } = await pool.query('SELECT * FROM messages');
+        return rows;
+    } catch (err) {
+        console.log("Error fetching messages:", err)
+        throw err;
+    }
+    
 }
-
-getMessagesFromDB();
-
-
 async function addMessageToDB(text, username, added) {
-    await pool.query('INSERT INTO messages (text, username, added) VALUES ($1, $2, $3)', [text, username, added])
+    try {
+        await pool.query('INSERT INTO messages (text, username, added) VALUES ($1, $2, $3)', [text, username, added])
+    } catch (err) {
+        console.log("Failed adding message: ", err);
+        throw err;
+    }
+    
 }
 
 async function getSingleMessageFromDB(messageId) {
-    const { rows } = await pool.query('SELECT * FROM messages WHERE id = $1', [messageId]);
-    return rows[0];
+    try {
+        const { rows } = await pool.query('SELECT * FROM messages WHERE id = $1', [messageId]);
+        return rows[0];
+    } catch (err) {
+        console.log("Failed fetching message: ", err);
+        throw err;
+    }
+    
 }
 
 
